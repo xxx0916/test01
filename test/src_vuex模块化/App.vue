567e2cc583f41a03746bcbc2106700229aa0027b
xjh <2604350081@qq.com>
2022-08-08 22:35:51 +0800

    first vue

---
+++ test/src_vuex模块化/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <CountInfo></CountInfo>
+        <hr>
+        <Person></Person>
+    </div>
+</template>
+
+<script>
+import CountInfo from "./components/CountInfo.vue";
+import Person from "./components/Person.vue";
+
+// 引入组件
+export default {
+    name: "App",
+    components: { CountInfo, Person },
+
+}
+</script>
+<style>
+.container,
+.foot {
+    display: flex;
+    justify-content: space-around;
+}
+
+h4 {
+    text-align: center;
+}
+</style>
